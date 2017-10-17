f7facbef6d2f3587f201ca340fc8760cf6ba107d
ameet.chaubal <ameet.chaubal@accenture.com>
2017-10-17 10:50:13 -0400

    added hash

--- src/main/scala/IIDRSparkProcessing.scala
+++ src/main/scala/IIDRSparkProcessing.scala
@@ -15,6 +15,7 @@ class IIDRSparkProcessing(sc: org.apache.spark.SparkContext, spark: org.apache.s
                           inputDirs: Array[String], schemaFile: String) {
 
   import java.io.File
+  import java.security.MessageDigest
 
   import org.apache.spark.sql.DataFrame
   import org.apache.spark.sql.functions._
@@ -27,16 +28,22 @@ class IIDRSparkProcessing(sc: org.apache.spark.SparkContext, spark: org.apache.s
   val METADATA_FILE_NAME = "meta.csv"
   val DATA_FORMAT = "csv"
   val DELIM = "|"
+  val IIDR_OUT_DIR = "iidr_post"
   val POST_COL_CNT = 1180
+  val PK_COL_CNT = 6
   // private vars
   private var _monolithDF: DataFrame = _
   private var _postDF: DataFrame = _
+  private var _finalDF: DataFrame = _
   private var _csvDF: DataFrame = _
   private var indAr: Array[Int] = _
 
   def indArray: Array[Int] = indAr
 
   def postdf: DataFrame = _postDF
+
+  def finaldf: DataFrame = _finalDF
+
 
   def csvdf: DataFrame = _csvDF
 
@@ -84,6 +91,20 @@ class IIDRSparkProcessing(sc: org.apache.spark.SparkContext, spark: org.apache.s
   }
   }
 
+  def cleanseStringData: (String => String) = { x => {
+    if (x == null) {
+      x
+    } else {
+      x.replace("\"", "").replace("\f", "").replaceAll("^\\s+", "").replaceAll("\\s+$", "")
+    }
+  }
+  }
+
+  def generatePKHash: (String => String) = { s => {
+    new String(MessageDigest.getInstance("MD5").digest(s.getBytes))
+  }
+  }
+
   def run(): Unit = {
     /**
       * a UDF to extract file name from abs path
@@ -102,12 +123,17 @@ class IIDRSparkProcessing(sc: org.apache.spark.SparkContext, spark: org.apache.s
       load(inputDirs: _*)
 
     indAr = createColIndexArray(_csvDF)
-
+    /**
+      * had this as well
+      * //    val postDF3 = postDF2.select(postDF2.columns.map(c => regexp_replace(postDF2.col(c), "\\s+$", "").alias(c)): _*)
+      * //    val postDF4 = postDF3.select(postDF3.columns.map(c => regexp_replace(postDF3.col(c), "^\\s+", "").alias(c)): _*)
+      * //    val postDF6 = postDF4.select(postDF4.columns.map(c => trim(postDF4.col(c)).alias(c)): _*)
+      **/
     val removeDoubleQuotes = udf((x: String) => {
       if (x == null) {
         x
       } else {
-        x.replace("\"", "").replace("\f", "").replaceAll("^\\s+", "").replaceAll("\\s+$", "")
+        x.replace("\"", "").replace("\f", "").replaceAll("^\\s+", "").replaceAll("\\s+$", "").trim
       }
     })
 
@@ -118,22 +144,23 @@ class IIDRSparkProcessing(sc: org.apache.spark.SparkContext, spark: org.apache.s
     //    val postDF1 = csvDF.select(ind map csvDF.columns map csvDF.col: _*)
     _postDF = _csvDF.select(indAr map _csvDF.columns map _csvDF.col: _*)
 
+    // can checkpoint if needed
+    //    _postDF.checkpoint(false)
     val postDF2 = _postDF.select(_postDF.columns.map(c => removeDoubleQuotes(_postDF.col(c)).alias(c)): _*)
-    val postDF3 = postDF2.select(postDF2.columns.map(c => regexp_replace(postDF2.col(c), "\\s+$", "").alias(c)): _*)
-    val postDF4 = postDF3.select(postDF3.columns.map(c => regexp_replace(postDF3.col(c), "^\\s+", "").alias(c)): _*)
-    // trim all columns
-    val postDF6 = postDF4.select(postDF4.columns.map(c => trim(postDF4.col(c)).alias(c)): _*)
+    val postDF7 = postDF2.withColumn("fileName", udfFileName(input_file_name()))
 
-    // add file name column
-    val postDF = postDF6.withColumn("fileName", udfFileName(input_file_name()))
+    val pkInd = (0 to PK_COL_CNT).toArray
+    val pkHashUDF = udf(generatePKHash)
+    _finalDF = postDF7.withColumn("pk_hash", pkHashUDF(concat_ws("~", pkInd map postDF7.columns map col: _*)))
+    _finalDF = postDF2
 
     // create a single column
     //concatenate, using smooch operator. converts a Seq of fieldNames to cols -> vararg
-    val monolithAddedDF = postDF.withColumn("monolith", concat_ws("|", postDF.schema.fieldNames.map(fName => postDF.col(fName)): _*))
+    val monolithAddedDF = _finalDF.
+      withColumn("monolith", concat_ws("|", _finalDF.schema.fieldNames.map(fName => _finalDF.col(fName)): _*))
     _monolithDF = monolithAddedDF.select(monolithAddedDF.col("monolith"))
 
-    _monolithDF.printSchema
-
     // write to HDFS with a single reducer
+    _monolithDF.write.text(IIDR_OUT_DIR)
   }
 }
